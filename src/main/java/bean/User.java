98d94893ca17d48fa9593d069906ab9e4e03ae43
spring <spring@qbserver.cn>
2019-03-29 21:49:41 +0800

    init

---
+++ src/main/java/bean/User.java
@@ -0,0 +1,38 @@
+package bean;
+
+/**
+ * 用户
+ * 
+ * @author Administrator
+ *
+ */
+public class User {
+	private String username;
+	private String mail;
+	private String searchBorrowingUrl;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getSearchBorrowingUrl() {
+		return searchBorrowingUrl;
+	}
+
+	public void setSearchBorrowingUrl(String searchBorrowingUrl) {
+		this.searchBorrowingUrl = searchBorrowingUrl;
+	}
+
+}
